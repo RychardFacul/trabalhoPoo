1fea901853b4771d7ae623f7f855f9444af9efe7
Maria Luiza Gonçalves <gmarialuiza081@gmail.com>
2024-03-30 13:01:21 -0400

    teste123

--- resources/views/categorias/apagar.blade.php
+++ resources/views/categorias/apagar.blade.php
@@ -5,7 +5,7 @@
     <div class="bg-body-tertiary p-5 rounded">
         <h1>Exclusão de Categoria</h1>
         <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                <label for="exampleFormControlInput1" class="form-label">Nome Categoria</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1">
             </div>
         </div>
