8b46ad2a0aa847412755d80c11937f21d7b08846
José Antoino <42252555+joseagnetoo@users.noreply.github.com>
2024-04-08 15:34:13 -0400

    Jose
    Primeira Atualização, testando customização das paginas

--- resources/views/estaticas/homepage.blade.php
+++ resources/views/estaticas/homepage.blade.php
@@ -1,4 +1,30 @@
 @extends('site')
 @section('section')
-    Está é a pagina de apresentação
+<!---Modified By Jose--->
+<link href="./css/home.css" rel="stylesheet">
+    
+<div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./img/Slider1.jpeg" class="d-block w-100 h-60" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./img/Slider2.jpg" class="d-block w-100 h-60" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./img/Slider3.jpg" class="d-block w-100 h-60" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+  <h1>BEM VINDO AO NOSSO MERCADO - NERDOLAS STORE</h1>
+
 @endsection
