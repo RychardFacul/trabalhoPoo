495c51a810a00bd865636a09c4e7fca1f60e1e2d
RychardProficional <rychardproficional@gmail.com>
2024-04-12 08:32:57 -0400

    seila

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,17 @@
 @extends('site')
 @section('title', 'Login')
-    
+@section('link')
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endsection
 @section('section')
+
     <form class="bg-body-tertiary p-5 rounded" method="POST" action="{{route('login')}}">
         @csrf
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">email</label>
             <input name="email" type="email" class="form-control" id="exampleFormControlInput1">
         </div>
-        <div class="mb-3">
+        <div class="mb-2">
             <label for="exampleFormControlTextarea1" class="form-label">Senha</label>
             <input name="senha" type="password" class="form-control" id="exampleFormControlInput1">
         </div>
