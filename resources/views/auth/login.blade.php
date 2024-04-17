44a5ea842c0a3df0b9f5a4b9ba552a6631096a22
José Antoino <42252555+joseagnetoo@users.noreply.github.com>
2024-04-17 00:03:47 -0400

    Commit Login OK

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,22 +5,23 @@
 @endsection
 @section('section')
 
-    <form class=" luiza w-100 m-auto form-container  p-5 rounded" method="POST" action="{{route('login')}}">
+    <form class=" formLogin w-100 m-auto p-5 rounded" method="POST" action="{{route('login')}}">
+        <div class="imgLogoNome"><img src="../../img/icone.png" alt="">SóGiro Supermercado</div>
         @csrf
-            <div class="formulario">
+        <div class="formulario">
             <label for="floatingInput" class="">Email</label>
-            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="your-emai@gmail.com" >
+            <input name="email" type="email" class="form-control inputForm" id="floatingInput" placeholder="your-emai@gmail.com" >
            
         </div>
-        <div class="maria">
+        <div>
             <label for="floatingInput" class="">Senha</label>
-            <input name="senha" type="password" class="form-control" id="floatingInput" placeholder="password" >
+            <input name="senha" type="password" class="form-control inputForm" id="floatingInput" placeholder="password" >
         </div>
         <div class="from-check text-start my-3">
             <input type="checkbox" class="form-check-input" id="flexCheckDefault">
             <label class="form-check-label" for="flexCheckDefault">lembra-me</label>
         </div>
-        <a href="{{route("cadastro")}}"><button class="btn azul w-100 py-2">cadastrar-se</button></a>
+        <button type="submit">Login</button>
     </form>
         
 @endsection
--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="./css/home.css" rel="stylesheet">
+        <link href="./css/base.css" rel="stylesheet">
         <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
