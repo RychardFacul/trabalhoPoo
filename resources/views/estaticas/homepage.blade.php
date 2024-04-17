b2ead9725f06f0c04bb47071848c5d40b53dadbd
0Fafnir <eduardosantana127@gmail.com>
2024-04-17 01:28:11 -0400

    eisssoirmao

--- resources/views/estaticas/homepage.blade.php
+++ resources/views/estaticas/homepage.blade.php
@@ -2,8 +2,48 @@
 @section('section')
 <!---Modified By Jose--->
 <link href="./css/home.css" rel="stylesheet">
-    
-<div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+
+<h1 class="text-center">BEM VINDO AO NOSSO MERCADO - SóGiro SUPERMERCADO</h1>
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="5000">
+      <img src="./img/arte principal.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="5000">
+      <img src="./img/Slider2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="5000">
+      <img src="./img/Slider3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!-- <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="./img/Slider1.jpeg" class="d-block w-100 h-60" alt="...">
@@ -23,9 +63,8 @@
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
-  </div>
+  </div> -->
 
-  <h1>BEM VINDO AO NOSSO MERCADO - SóGIRO SUPERMERCADO</h1>
   
   <a href="{{route("produtos")}}"><button class="btn azul w-100 py-2">veja nossos produtos</button></a>
 
